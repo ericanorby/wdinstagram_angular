505a9ae47301b48fc01dd689b6b8f0fff6dc8013
Erica Norby <ericanorby@gmail.com>
2017-03-08 13:46:57 -0500

    bootstrapped angular and created index controller

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+    .module("wdinstagram", [
+      "ui.router"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ])
+    .controller("InstaIndexController", [
+      InstaIndexControllerFunction
+    ])
+
+    function RouterFunction($stateProvider){
+      $stateProvider
+      .state("instaIndex", {
+        url: "/wdinstagrams",
+        templateUrl: "js/ng-views/index.html",
+        controller: "InstaIndexController",
+        controllerAs: "vm"
+      })
+    }
+
+    function InstaIndexControllerFunction(){
+      this.instas = instaData
+    }
+
 })();
